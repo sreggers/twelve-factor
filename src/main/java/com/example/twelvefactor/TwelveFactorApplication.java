ab4d47b6015f80487e6c02d77f52a345a173441f
Reggie Ebendal <reggie.ebendal@gmail.com>
2019-01-11 16:31:00 +0100

    Initial Spring Boot app

---
+++ src/main/java/com/example/twelvefactor/TwelveFactorApplication.java
@@ -0,0 +1,14 @@
+package com.example.twelvefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveFactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwelveFactorApplication.class, args);
+	}
+
+}
+
